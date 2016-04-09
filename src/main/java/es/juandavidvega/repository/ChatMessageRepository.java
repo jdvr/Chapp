f2415df5663cd72d95f0693d1808e707dace6a3e
Juan David Vega <jdvr.93@hotmail.com>
2016-04-09 14:37:54 +0200

    add hsql embedded configuration

---
+++ src/main/java/es/juandavidvega/repository/ChatMessageRepository.java
@@ -0,0 +1,4 @@
+package es.juandavidvega.repository;
+
+public interface ChatMessageRepository {
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table Account (
+  id identity,
+  content varchar (50) not null,
+  sender varchar (50) not null,
+  sendDate date not null
+);
